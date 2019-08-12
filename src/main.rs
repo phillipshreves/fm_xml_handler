b2566c3503b5384e59a5a610712914fd8bf360c9
Phillip Shreves <pshreves@highpointgo.com>
2019-08-12 10:33:02 -0500

    testing quick_xml implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+extern crate quick_xml;
 
+use quick_xml::Reader;
+use quick_xml::events::Event;
+
+fn main() {
+    let xml = r#"<tag1 att1 = "test">
+                    <tag2><!--Test comment-->Test</tag2>
+                    <tag2>
+                        Test 2
+                    </tag2>
+                </tag1>"#;
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+
+}
+
+fn stream(xml: str){
+    println!("{}", String::from_str(xml));
 }
