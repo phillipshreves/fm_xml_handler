a53eac9e42d4dce37182f177c036e55539a10cfb
Phillip Shreves <hi@phillips.work>
2019-08-13 17:21:38 -0500

    Added debugger files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 extern crate quick_xml;
 
-fn main(){
-    use quick_xml::Reader;
+fn main() {
     use quick_xml::events::Event;
+    use quick_xml::Reader;
 
-    let file_path = "/Users/phillipshreves/AltDesktop/Developer Documents/xml_parsing/xml_test.xml";
+    //let file_path = "/Users/phillipshreves/AltDesktop/Developer Documents/xml_parsing/xml_test.xml";
+    let file_path = "/home/phillip/xml_test.xml";
     let mut reader = Reader::from_file(file_path).unwrap();
+   /* let xml = r#"<tag1 att1 = "test">
+                <tag2><!--Test comment-->Test</tag2>
+                <tag2>
+                    Test 2
+                </tag2>
+            </tag1>"#;
+
+    let mut reader = Reader::from_str(xml);*/
     reader.trim_text(true);
 
     let mut count = 0;
@@ -15,19 +24,21 @@ fn main(){
     // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
     loop {
         match reader.read_event(&mut buf) {
-        // for triggering namespaced events, use this instead:
-        // match reader.read_namespaced_event(&mut buf) {
+            // for triggering namespaced events, use this instead:
+            // match reader.read_namespaced_event(&mut buf) {
             Ok(Event::Start(ref e)) => {
-            // for namespaced:
-            // Ok((ref namespace_value, Event::Start(ref e)))
+                // for namespaced:
+                // Ok((ref namespace_value, Event::Start(ref e)))
+                let name = e.name();
                 match e.name() {
-                    b"tag1" => println!("attributes values: {:?}",
-                                        e.attributes().map(|a| a.unwrap().value)
-                                        .collect::<Vec<_>>()),
+                    b"tag1" => println!(
+                        "attributes values: {:?}",
+                        e.attributes().map(|a| a.unwrap().value).collect::<Vec<_>>()
+                    ),
                     b"tag2" => count += 1,
                     _ => (),
                 }
-            },
+            }
             // unescape and decode the text event using the reader encoding
             Ok(Event::Text(e)) => txt.push(e.unescape_and_decode(&reader).unwrap()),
             Ok(Event::Eof) => break, // exits the loop when reaching end of file
