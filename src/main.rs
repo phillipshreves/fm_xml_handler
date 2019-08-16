8697947e2af9171ca09e78d2a0fca8e8b8d36b80
Phillip Shreves <pshreves@highpointgo.com>
2019-08-16 09:16:03 -0500

    Finished XML escape and build functions

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,11 @@ use std::fs;
 
 #[derive(Debug)]
 pub struct Field {
-    empty_ok: String,
-    max_repeat: String,
     name: String,
     field_type: String,
 }
 
+//These static variables will be used to identify escape characters
 static AMPERSAND: u8 = '&' as u8;
 static LESS_THAN: u8 = '<' as u8;
 static GREATER_THAN: u8 = '>' as u8;
@@ -19,13 +18,15 @@ static QUOTE_DOUBLE: u8 = '\"' as u8;
 static QUOTE_SINGLE: u8 = '\'' as u8;
 
 fn main() -> std::io::Result<()> {
-
-    let mut record_hashmap = HashMap::new();
-    let mut fields = Vec::new();
-
+//These parameters need to be changed at runtime
     let table_names = vec![String::from("Contacts"), String::from("ContactDetails")];
     let export_folder = String::from("/Users/phillipshreves/Desktop/");
 
+//Variable setup
+    let mut record_hashmap = HashMap::new();
+    let mut fields = Vec::new();
+
+//Now we get the record data, parse it out, and build the xml
     let mut counter_tables = 0;
     record_hashmap = loop {
         let table_name = &table_names[counter_tables];
@@ -39,6 +40,7 @@ fn main() -> std::io::Result<()> {
         }
     };
 
+//Now we get the field metadata, parse it out, and build the xml
     let mut counter_tables = 0;
     fields = loop {
         let table_name = &table_names[counter_tables];
@@ -52,17 +54,9 @@ fn main() -> std::io::Result<()> {
         }
     };
 
-    //Example Results:
-    //println!("{:#?}", record_hashmap["19991"]);
-    //println!("{:#?}", fields[0]);
-    //println!("{:#?}", xml_field_metadata(fields));
-    //println!("{}", xml_record_data(record_hashmap));
-
+//Create the XML
     let xml_to_write = format!("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 <FMPXMLRESULT xmlns=\"http://www.filemaker.com/fmpxmlresult\">
-    <ERRORCODE>0</ERRORCODE>
-    <PRODUCT BUILD=\"07-05-2019\" NAME=\"FileMaker\" VERSION=\"ProAdvanced 18.0.2\"/>
-    <DATABASE DATEFORMAT=\"M/d/yyyy\" LAYOUT=\"\" NAME=\"TM_Dev.fmp12\" RECORDS=\"53757\" TIMEFORMAT=\"h:mm:ss a\"/>
     <METADATA>
     {}
     </METADATA>
@@ -71,11 +65,13 @@ fn main() -> std::io::Result<()> {
     </RESULTSET>
 </FMPXMLRESULT>", xml_field_metadata(fields), xml_record_data(record_hashmap)) ;
 
+//Write XML to file
     fs::write(format!("{}import_test.xml", export_folder), xml_to_write)?;
     Ok(())
 }
 
 fn escape_xml(data: String) -> String {
+    //Check if we need to escape any characters, return early if we don't.
     let mut escape_characters = false;
     if data.contains("&")
         | data.contains("<")
@@ -89,6 +85,7 @@ fn escape_xml(data: String) -> String {
         return data;
     }
 
+    //change character to escape string
     let mut data_escaped = String::new();
     for character in data.chars(){
         let char_u8 = character as u8 ;
@@ -104,11 +101,10 @@ fn escape_xml(data: String) -> String {
         } else if char_u8 == QUOTE_SINGLE {
             character_escaped = "&apos;".to_string();
         };
-        // Need to update this to concat a string instead of a char
-        //data_escaped.push(character_escaped)
+        data_escaped = format!("{}{}", data_escaped, character_escaped);
     };
 
-    return data;
+    return data_escaped;
 }
 
 fn field_metadata(
@@ -117,31 +113,22 @@ fn field_metadata(
     mut fields: Vec<Field>,
 ) -> Vec<Field> {
     // The fields vector will contain all of the metadata for the fields in order so that when we pull them later we can line them up with the data /*
-    let field_example = Field {
-        empty_ok: String::from("YES"),
-        max_repeat: String::from("1"),
-        name: String::from("ID_Contact"),
-        field_type: String::from("NUMBER"),
-    };
-    fields.push(field_example);
     let xml = fs::read_to_string(xml_filepath).expect("file error");
 
     let root: Element = xml.parse().unwrap();
     for table in root.children() {
         if table.name() == "METADATA" {
             for field in table.children() {
+
                 let mut field_structure = Field {
-                    empty_ok: String::new(),
                     field_type: String::new(),
-                    max_repeat: String::new(),
                     name: String::new(),
                 };
+
                 for attribute in field.attrs() {
                     let a_name = attribute.0;
                     let a_value = attribute.1;
                     match a_name {
-                        "EMPTYOK" => field_structure.empty_ok = String::from(a_value),
-                        "MAXREPEAT" => field_structure.max_repeat = String::from(a_value),
                         "NAME" => {
                             field_structure.name =
                                 String::from([table_name, "__", a_value].concat())
@@ -150,6 +137,7 @@ fn field_metadata(
                         _ => (),
                     }
                 }
+
                 fields.push(field_structure);
             }
         }
@@ -169,7 +157,9 @@ fn update_record_hashmap(
     for table in root.children() {
         if table.name() == "RESULTSET" {
             for record in table.children() {
+
                 let mut record_data = Vec::new();
+                
                 for field in record.children() {
                     for value in field.children() {
                         let mut data = value.text();
@@ -177,10 +167,11 @@ fn update_record_hashmap(
                         record_data.push(data);
                     }
                 }
+
                 let record_id = record_data[0].clone();
                 let mut record_vec = Vec::new();
                 if record_hashmap.contains_key(&record_id) {
-                    record_vec.extend(record_hashmap[&record_id].clone());
+                    record_vec = record_hashmap[&record_id].clone();
                 };
                 record_vec.extend(record_data);
                 record_hashmap.insert(record_id, record_vec);
@@ -192,13 +183,13 @@ fn update_record_hashmap(
 }
 
 fn xml_field_metadata(field_vector: Vec<Field>) -> String {
-    //This sets up the XML which determines what field contains the data found in xml_record_data
+    //This sets up the METADATA XML which determines what field the data pertains to
     let mut xml = String::new();
 
     for field in &field_vector {
         xml = format!(
-            "{}<FIELD EMPTYOK=\"{}\" MAXREPEAT=\"{}\" NAME=\"{}\" TYPE=\"{}\"/>",
-            xml, field.empty_ok, field.max_repeat, field.name, field.field_type
+            "{}<FIELD NAME=\"{}\" TYPE=\"{}\"/>",
+            xml, field.name, field.field_type
         );
     }
 
@@ -206,7 +197,7 @@ fn xml_field_metadata(field_vector: Vec<Field>) -> String {
 }
 
 fn xml_record_data(record_data: HashMap<String, Vec<String>>) -> String {
-    //This sets up the XML for the record data
+    //This sets up the RESULTSET XML for the record data
     let mut xml = String::new();
 
     for record in record_data.values() {
